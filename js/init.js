6b3c155a6856dd637051b106d1a4b3fcda7eb5e6
DanielPintilei <printrelei@gmail.com>
2014-11-24 18:05:44 +0200

    neww

--- js/init.js
+++ js/init.js
@@ -1,9 +1,3 @@
-/*
-	Prologue by HTML5 UP
-	html5up.net | @n33co
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
-*/
-
 (function($) {
 
 	skel.init({
@@ -24,7 +18,7 @@
 					position: 'top-left',
 					side: 'left',
 					animation: 'pushX',
-					width: 240,
+					width: 70,
 					height: '100%',
 					clickToHide: true,
 					html: '<div data-action="moveElement" data-args="header"></div>',

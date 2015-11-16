2bbdbf5fb5b10777f4bb5d8f23ee31ded2851720
Daniel Pintilei <danielpintilei@outlook.com>
2015-11-16 20:25:00 +0200

    last

--- js/main.js
+++ js/main.js
@@ -1,13 +1,10 @@
-<script>
-    $('a').click(function(){
-    $('html, body').animate({
-    scrollTop: $( $(this).attr('href') ).offset().top}, 500);
-    return false;
+    $('a').click(function () {
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 500);
+        return false;
     });
-</script>
 
-<script>
-$(window).scroll(function(){
-    $("#landing").css("opacity", 1 - $(window).scrollTop() / 100);
-});
-</script>
+    $(window).scroll(function () {
+        $("#landing").css("opacity", 1 - $(window).scrollTop() / 100);
+    });
